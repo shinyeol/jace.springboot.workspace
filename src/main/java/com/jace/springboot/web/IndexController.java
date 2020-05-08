774cdae2a8acd0c6af52396b3e97ab969e463fc5
SHINYEOL <lsl1117@hanmail.net>
2020-05-08 11:16:55 +0900

    oauth 추가

--- src/main/java/com/jace/springboot/web/IndexController.java
+++ src/main/java/com/jace/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.jace.springboot.web;
 
 import com.jace.springboot.service.PostsService;
+import com.jace.springboot.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +22,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
